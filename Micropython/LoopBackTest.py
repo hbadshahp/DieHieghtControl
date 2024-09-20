36b679e301d4253145d344c88d727bfe26480a4b
hbadshahph@gmail.com <hbadshahph@gmail.com>
2024-09-20 13:05:28 +0530

    Added modbus pre transmission and post transmission logic of pin high low

--- Micropython/LoopBackTest.py
+++ Micropython/LoopBackTest.py
@@ -5,18 +5,22 @@ import ubinascii
 from umqtt.simple import MQTTClient
 
 # Modbus settings
-
+MODBUS_DIR_PIN = Pin(23, Pin.OUT)
 MODBUS_RX_PIN = 16
 MODBUS_TX_PIN = 17
 MODBUS_SERIAL_BAUD = 9600
 uart = UART(2, baudrate=MODBUS_SERIAL_BAUD, tx=MODBUS_TX_PIN, rx=MODBUS_RX_PIN)
 
+# Function prototypes
+def modbus_pre_transmission():
+    MODBUS_DIR_PIN.off()
 
+def modbus_post_transmission():
+    MODBUS_DIR_PIN.on()
     
-
+modbus_pre_transmission()
 uart.write(b'hello')
 time.sleep(1)
 response = uart.read(5)
 print(f"Loopback response: {response}")
-
-
+modbus_post_transmission()
